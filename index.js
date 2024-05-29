ee902aed9fba2935d58e7641a42d7f59b1deab7c
DJoverdant <166723698+DJoverdant@users.noreply.github.com>
2024-05-29 10:33:47 -0300

    update 0.2

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function validate(){
     var question = document.getElementById("question").value; 
 
     if (user == "User" && question == "sim") { 
-        window.location.href = "pag3.html"; 
-    } 
-
-    else if (user == "User" && question == "nao") { 
         window.location.href = "pag2.html"; 
+    } 
+ 
+    else if (user == "User" && question == "nao") { 
+        window.location.href = "pag3.html"; 
     } 
 
     else { 
@@ -18,8 +18,4 @@ function validate(){
 
 function home(){
     window.location.href = "index.html"
-}
-
-function question(){
-    window.location.href = "question.html"
 }
